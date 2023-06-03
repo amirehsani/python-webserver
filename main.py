04b33a552306691cc54f7a1ccfc91a3b0a02c3a3
Amir Ehsani <65291639+amirehsani@users.noreply.github.com>
2023-06-03 12:52:32 +0330

    Updated main.py a bit using Chat GPT

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import time
-
-
-HOST = "127.0.0.1"
-PORT = 9999
 
 
 class MyHTTP(BaseHTTPRequestHandler):
 
-	# override
-	# this function specifies how we handle GET requests
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
+    # override
+    # this function specifies how we handle GET requests
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
 
-		self.wfile.write(bytes("<html><body><h1> Hello World! </h1></body></html>", "utf-8"))
+            self.wfile.write(bytes("<html><body><h1> Hello World! </h1></body></html>", "utf-8"))
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
 
-	# override
-	# handling POST requests
-	def do_POST(self):
-		self.send_response(200)
-		self.send_header("Content-type", "application/json")
-		self.end_headers
+            self.wfile.write(bytes("404 Not Found", "utf-8"))
 
-		# some instruction
+    # override
+    # handling POST requests
+    def do_POST(self):
+        content_length = int(self.headers["Content-Length"])
+        post_data = self.rfile.read(content_length).decode("utf-8")
+
+        # Process the post_data as needed
+        # ...
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
+        self.wfile.write(bytes("POST request processed", "utf-8"))
 
 
+def run(server_class=HTTPServer, handler_class=MyHTTP, host="localhost", port=8020):
+    server_address = (host, port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Server is running on {host}:{port}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+        print("Server stopped")
 
 
-server = HTTPServer((HOST, PORT), MyHTTP)
-print(f"Server is running on {HOST}:{PORT}")
-
-server.serve_forever()
-
-
+if __name__ == "__main__":
+    run()
